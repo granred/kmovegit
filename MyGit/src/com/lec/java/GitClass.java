404608a4df7e7ca9a7b636f2dfaddb29a29a13c4
tjdgn <tjdgn@DESKTOP-3QQPGEI>
2018-11-23 11:50:04 +0900

    첫업로드입니다

---
+++ MyGit/src/com/lec/java/GitClass.java
@@ -0,0 +1,14 @@
+package com.lec.java;
+/*
+ * このclassはtest用です。
+ * 
+ * 
+ * */
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		System.out.println("GitHub 에 올리기");
+	}
+
+}
